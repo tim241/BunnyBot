be01fb2490ac1a8f5f27e730dc42f7a4a0f20117
Tim Wanders <timwanders241@gmail.com>
2018-08-29 20:22:13 +0200

    Added WIP Discord v6 library
    This Discord library wraps around the HttpClient library
    
    * Added DiscordInfo
        Allows you to set the token, name, url and version
    
    * Added DiscordBot
        DiscordBot will handle the Discord v6 API

---
+++ Source/Discord/v6/DiscordBot.cs
@@ -0,0 +1,55 @@
+using System;
+using Http;
+
+namespace Discord.v6
+{
+    public class DiscordBot : IDisposable
+    {
+        private static string discordUrl = "https://discordapp.com";
+        private string name { get; set; }
+        private string url { get; set; }
+        private string version { get; set; }
+        private string token { get; set; }
+        public DiscordBot(DiscordInfo info)
+        {
+            name = info.Name;
+            url = info.Url;
+            version = info.Version;
+            token = info.Token;
+        }
+        public void Connect()
+        {
+            HttpHeader header = new HttpHeader
+            {
+                GET = "/api/v6/users/@me",
+                HOST = "discordapp.com",
+                USER_AGENT = $"{name} ({url}, {version})",
+                AUTORIZATION = $"Bot {token}"
+            };
+            using (HttpClient client = new HttpClient(discordUrl, header, true))
+            {
+                string line;
+
+
+                while ((line = client.ReadLine()) != null)
+                {
+                    if (client.HttpCode != 200)
+                    {
+                        Console.WriteLine("invalid response");
+                        Environment.Exit(1);
+                    }
+                    Console.WriteLine(line);
+                }
+
+                // Throw away null line
+                client.ReadLine();
+
+                client.SendHeader(header);
+            }
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Source/Discord/v6/DiscordInfo.cs
@@ -0,0 +1,10 @@
+namespace Discord.v6
+{
+    public class DiscordInfo
+    {
+        public string Token { get; set; }
+        public string Url { get; set; }
+        public string Version { get; set; }
+        public string Name { get; set; }
+    }
+}
