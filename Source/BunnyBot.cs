467e2a21af5f378ffc9745caac6dc689ec22defb
Tim Wanders <timwanders241@gmail.com>
2018-08-29 23:06:53 +0200

    [BunnyBot] Some small changes

--- Source/BunnyBot.cs
+++ Source/BunnyBot.cs
@@ -2,24 +2,36 @@
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-
+using System.Text;
 using Http;
+using Discord.v6;
 
 namespace BunnyBot
 {
     class Program
     {
+        public static string BotToken = Environment.GetEnvironmentVariable("BUNNYBOT_TOKEN");
+        public static string BunnyBotVersion = "1.0";
         static void Main(string[] args)
         {
-            HttpHeader header = new HttpHeader { 
-                GET = "/search?q=HelloWorld",
-                HOST = "www.google.com"
+            if (string.IsNullOrEmpty(BotToken))
+            {
+                Console.WriteLine("Please set BUNNYBOT_TOKEN with the bot token");
+                Environment.Exit(1);
+            }
+
+            DiscordInfo info = new DiscordInfo
+            {
+                Token = BotToken,
+                Name = "BunnyBot",
+                Url = "https://gitlab.com/tim241/BunnyBot",
+                Version = BunnyBotVersion
             };
 
-           using(HttpClient client = new HttpClient("www.google.com", header))
-           {
-               client.Connect();
-           }
+            using (DiscordBot bot = new DiscordBot(info))
+            {
+                bot.Connect();
+            }
         }
     }
 }
